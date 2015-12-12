9e8ea532ecbd8f0edb003f9e8601b9f5c6a8784d
Maros Kollar <maros@k-1.com>
2015-12-12 15:41:23 +0100

    Initial comit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Zway-BaseModule
+
+Module which provides many useful functions for other automation modules.
+Has no functionality on ist own
+
+# Configuration
+
+No configuration required
+
+# Virtual Devices
+
+No virtual device is created
+
+# Events
+
+No events are emitted
+
+# Installation
+
+```shell
+cd /opt/z-way-server/automation/modules
+git clone https://github.com/maros/Zway-BaseModule.git BaseModule --branch latest
+```
+
+To update or install a specific version
+```shell
+cd /opt/z-way-server/automation/modules/BaseModule
+git fetch --tags
+# For latest released version
+git checkout tags/latest
+# For a specific version
+git checkout tags/1.02
+# For development version
+git checkout -b master --track origin/master
+```
+
+# License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any 
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
---
+++ index.js
@@ -0,0 +1,189 @@
+/*** BaseModule Z-Way HA module *******************************************
+
+Version: 1.00
+(c) Maroš Kollár, 2015
+-----------------------------------------------------------------------------
+Author: Maroš Kollár <maros@k-1.com>
+Description:
+    This module provides many helpful functions for zway automation module
+    developers.
+
+******************************************************************************/
+
+function BaseModule (id, controller) {
+    // Call superconstructor first (AutomationModule)
+    BaseModule.super_.call(this, id, controller);
+    
+    this.langFile       = undefined;
+}
+
+inherits(BaseModule, AutomationModule);
+
+_module = BaseModule;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+BaseModule.prototype.init = function (config) {
+    var self = this;
+    self.langFile = self.controller.loadModuleLang(self.getName());
+};
+
+BaseModule.prototype.stop = function () {
+    var self = this;
+    BaseModule.super_.prototype.stop.call(this);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+BaseModule.prototype.log = function(message) {
+    if (undefined === message) return;
+    console.logJS(this);
+    console.log('['+this.getName()+'-'+this.id+'] '+message);
+};
+
+BaseModule.prototype.error = function(message) {
+    if (undefined === message) message = 'An unknown error occured';
+    if (null !== log.caller) {
+        console.error('['+this.getName()+'_'+self.id+'] '+message+' at '+log.caller.name);
+    } else {
+        console.error('['+this.getName()+'_'+self.id+'] '+message);
+    }
+};
+
+Homely.prototype.getPresence = function() {
+    var self = this;
+    
+    var value = self.getDeviceValue([
+        ['probeType','=','Presence']
+    ]);
+    
+    if (typeof(value) === 'string' && value === 'on') {
+        return true;
+    }
+    self.log('Away!');
+    return false;
+};
+
+BaseModule.prototype.compareDevice = function(vDev,criterias) {
+    var self = this;
+
+    var match;
+    _.each(criterias,function(criteria) {
+        if (match !== false) {
+            var matchKey        = criteria[0];
+            var matchComparison = criteria[1];
+            var matchValue      = criteria[2];
+            var value;
+            if (matchKey === 'zwaveId') {
+                var result = vDev.id.match(/^ZWayVDev_zway_(\d+)-\d+-/m);
+                if (result === null) {
+                    match = false;
+                    return;
+                }
+                value = parseInt(result[1],10);
+            } else {
+                value = vDev.get(matchKey);
+            }
+            var matchCriteria;
+            if (typeof(value) === 'object') {
+                matchCriteria = (value.indexOf(matchValue) === -1) ? false:true;
+            } else if (typeof(value) !== 'undefined') {
+                if (matchKey === 'metrics:level') {
+                    if (vDev.get('deviceType') === 'switchBinary') {
+                        if (typeof(matchValue) === 'number') {
+                            matchValue = matchValue === 0 ? 'off':'on';
+                        } else if (typeof(matchValue) === 'boolean') {
+                            matchValue = matchValue ? 'on':'off';
+                        }
+                    } else if (vDev.get('deviceType') === 'switchMultilevel') {
+                        if (typeof(matchValue) === 'string') {
+                            matchComparison = matchValue === 'on' ? '!=':'=';
+                            matchValue = 0;
+                        } else if (typeof(matchValue) === 'boolean') {
+                            matchComparison = matchValue ? '!=':'=';
+                            matchValue = 0;
+                        }
+                    }
+                }
+                matchCriteria = (matchValue == value) ? true:false;
+            }
+            if (typeof(matchCriteria) === 'boolean') {
+                switch (matchComparison) {
+                    case('='):
+                        match = matchCriteria;
+                        break;
+                    case('!='):
+                        match = !matchCriteria;
+                        break;
+                }
+            } else {
+                match = false;
+            }
+        }
+    });
+    return match;
+};
+
+BaseModule.prototype.getDevices = function(criterias) {
+    var self = this;
+    
+    var devices = [];
+    self.controller.devices.each(function(vDev) {
+        var match = self.compareDevice(vDev,criterias);
+        if(match === true) {
+            devices.push(vDev);
+        }
+    });
+    return devices;
+};
+
+BaseModule.prototype.getDevice = function(criterias) {
+    var self = this;
+    
+    var device;
+    self.controller.devices.each(function(vDev) {
+        if (typeof(device) === 'undefined') {
+            var match = self.compareDevice(vDev,criterias);
+            if(match === true) {
+                device = vDev;
+            }
+        }
+    });
+    return device;
+};
+
+BaseModule.prototype.getDeviceValue = function(criterias,key) {
+    var self = this;
+    key = key || 'metrics:level';
+    var device = self.getDevice(criterias);
+    if (typeof(device) === 'undefined') {
+        self.error('Could not find device');
+    } else {
+        return device.get(key);
+    }
+};
+
+BaseModule.prototype.performCommandDevices = function(criterias,command,args,auto) {
+    var self = this;
+    args = args || {};
+    
+    var devices = [];
+    self.controller.devices.each(function(vDev) {
+        var match = self.compareDevice(vDev,criterias);
+        
+        if(match === true) {
+            devices.push(vDev);
+            if (typeof(command) !== 'undefined') {
+                vDev.performCommand(command,args);
+            }
+            if (typeof(auto) === 'boolean') {
+                vDev.set('metrics:auto',auto);
+            }
+        }
+    });
+    return devices;
+};
