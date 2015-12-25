64a2a1b3cc7808c0762adf5bb8c044de346b3303
Maros Kollar <maros@k-1.com>
2015-12-25 21:33:43 +0100

    Add stacktrace to error method

--- index.js
+++ index.js
@@ -51,11 +51,8 @@ BaseModule.prototype.log = function(message) {
 
 BaseModule.prototype.error = function(message) {
     if (undefined === message) message = 'An unknown error occured';
-    //if (null !== this.error.caller) {
-    //    console.error('['+this.getName()+'_'+this.id+'] '+message+' at '+this.error.caller.name);
-    //} else {
-        console.error('['+this.getName()+'_'+this.id+'] '+message);
-    //}
+    var error = new Error(message);
+    console.error('['+this.getName()+'_'+this.id+'] '+message+' at '+error.stack);
 };
 
 /* Presence helper functions */
