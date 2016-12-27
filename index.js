66824e020280b3b93be8583ca7a54482f4660600
Maros Kollar <maros@k-1.com>
2016-12-27 21:40:22 +0100

    Update copyright

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*** BaseModule Z-Way HA module *******************************************
 
 Version: 1.09
-(c) Maroš Kollár, 2015
+(c) Maroš Kollár, 2015-2017
 -----------------------------------------------------------------------------
 Author: Maroš Kollár <maros@k-1.com>
 Description:
